662f47468fa0f0e826f3b7f5c53ef524ad3f9214
iansedano <iansedano@gmail.com>
2020-04-07 16:44:39 +0200

    adding life rules, buggy af

--- script.js
+++ script.js
@@ -3,10 +3,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-//CANVAS Background
-ctx.beginPath();
-ctx.fillStyle = '#ffd1b3';
-ctx.fillRect(1, 1, 498, 498);
+
 
 
 
@@ -17,23 +14,31 @@ var gridSize = 9;
 var pad = 20.5;
 var gridSpacing = 50;
 
-ctx.beginPath();
-//vertical lines
-for (var i = 0; i < gridSize; i++) {
-	ctx.moveTo(pad + (gridSpacing * i), pad);
-	ctx.lineTo(pad + (gridSpacing * i), pad + ((gridSize - 1) * gridSpacing));
-}
-//horizontal lines
-for (var i = 0; i < gridSize; i++) {
-	ctx.moveTo(pad, pad + (gridSpacing * i));
-	ctx.lineTo( pad + ((gridSize - 1) * gridSpacing), pad + (gridSpacing * i));
-}
-ctx.stroke();
+function drawBoard() {
+	
+	//CANVAS Background
+	ctx.beginPath();
+	ctx.fillStyle = '#ffd1b3';
+	ctx.fillRect(1, 1, 498, 498);
 
-// Drawing tengen star point
-var tengen = new boardRef(5, 5)
-drawDot(tengen.point);
+	//GRID
+	ctx.beginPath();
+	//vertical lines
+	for (var i = 0; i < gridSize; i++) {
+		ctx.moveTo(pad + (gridSpacing * i), pad);
+		ctx.lineTo(pad + (gridSpacing * i), pad + ((gridSize - 1) * gridSpacing));
+	}
+	//horizontal lines
+	for (var i = 0; i < gridSize; i++) {
+		ctx.moveTo(pad, pad + (gridSpacing * i));
+		ctx.lineTo( pad + ((gridSize - 1) * gridSpacing), pad + (gridSpacing * i));
+	}
+	ctx.stroke();
 
+	// Drawing tengen star point
+	var tengen = new boardRef(5, 5)
+	drawDot(tengen.point);
+}
 // ++ CLICK MAP ++
 
 var clickMapArray = new Array(gridSize);
@@ -58,18 +63,18 @@ for (var i = 0; i < gridSize; i++) {
 /// draws rectangles
 for (var i in clickMapArray) {
 	for (var j in clickMapArray[i]) {
-		drawClickRect(clickMapArray[i][j]);
+//		drawClickRect(clickMapArray[i][j]);
 	}
 }
 
-
+/*
 function drawClickRect(point) {
 	ctx.beginPath();
 	ctx.fillStyle = '#ffff99';
 	ctx.fillRect(point.y - (boxSize/2), point.x - (boxSize/2), boxSize, boxSize);
 	ctx.stroke();
 	}
-
+*/
 
 // Click listening
 
@@ -80,50 +85,28 @@ function getCursorPosition(canvas, event) {
     return new point(x, y);
 }
 
-canvas.addEventListener('mousedown', function(e) {
-    newStoneRef = getBoardRef(getCursorPosition(canvas, e))
-    if (newStoneRef != "") {
-    	placeStone(newStoneRef)
-    }
-})
 
-function placeStone(boardRef) {
-	newStone = new stone(boardRef.bx, boardRef.by, playerTurn, moveIndex)
-    drawStone(newStone)
-    if (playerTurn == 'black') {
-    	playerTurn = 'white'
-    } else {
-    	playerTurn = 'black'
-    }
-    moveIndex += 1
-}
-	
-function getBoardRef(point) {
-	for (var i = 0; i < gridSize; i++) {
-		for (var j = 0; j < gridSize; j++) {
-			var xMin = clickMapArray[i][j].x - (boxSize / 2)
-			var xMax = clickMapArray[i][j].x + (boxSize / 2)
-			var yMin = clickMapArray[i][j].y - (boxSize / 2)
-			var yMax = clickMapArray[i][j].y + (boxSize / 2)
-			if (point.x >= xMin && point.x <= xMax && point.y >= yMin && point.y <= yMax) {
-				return new boardRef(i + 1, j + 1);
-				break;
-			}
-		}
-	}
-}
 
 // GAME STATE
 
 // Array of board
-var gameState = Array(gridSize).fill(Array(gridSize).fill(""));
 
-
+var gameState = new Array(gridSize);
+for (var i = 0; i < gridSize; i++) {
+	gameState[i] = new Array(gridSize);
+}
 
 // TESTING
 
-playerTurn = "black";
-moveIndex = 1;
-groupIndex = 1;
+drawBoard();
 
+playerTurn = "black";
+moveIndex = 0;
+
+canvas.addEventListener('mousedown', function(e) {
+    newStoneRef = getBoardRef(getCursorPosition(canvas, e))
+    if (newStoneRef != undefined) {
+    	placeStone(newStoneRef)
+    }
+})
 
--- stones.js
+++ stones.js
@@ -13,23 +13,19 @@ class boardRef {
 	}
 }
 
+var allStones = [];
 class stone {
 	constructor(boardX, boardY, string, moveIndex) {
 		this.boardRef = new boardRef(boardX, boardY);
-		this.alive = true;
 		this.colour = string
-		this.group = ""
 		this.moveIndex = moveIndex
 		this.point = findCoordinate(this.boardRef)
+		allStones.push(this);
 	}
 }
 
-class group {
-	constructor() {
-		this.index = groupIndex
-		groupIndex += 1
-	}
-}
+
+
 
 function findCoordinate(boardRef) {
 	var x = pad + ((boardRef.bx - 1) * gridSpacing)
@@ -60,3 +56,131 @@ function changeTurn() {
 		playerTurn = "black"
 	}
 }
+
+function placeStone(boardRef) {
+	newStone = new stone(boardRef.bx, boardRef.by, playerTurn, moveIndex)
+    drawStone(newStone)
+    gameState[boardRef.bx - 1][boardRef.by - 1] = newStone
+    changeTurn()
+    moveIndex += 1
+    for (var i = 0; i < allStones.length; i++) {
+		checkLife(allStones[i]);
+	}
+}
+
+function getBoardRef(point) {
+	for (var i = 0; i < gridSize; i++) {
+		for (var j = 0; j < gridSize; j++) {
+			var xMin = clickMapArray[i][j].x - (boxSize / 2)
+			var xMax = clickMapArray[i][j].x + (boxSize / 2)
+			var yMin = clickMapArray[i][j].y - (boxSize / 2)
+			var yMax = clickMapArray[i][j].y + (boxSize / 2)
+			if (point.x >= xMin && point.x <= xMax && point.y >= yMin && point.y <= yMax) {
+				return new boardRef(i + 1, j + 1);
+				break;
+			}
+		}
+	}
+}
+
+
+function checkLife(stoneToCheck) {
+	
+
+	var lifePoints = 0;
+
+	var N = "unassigned";
+	var E = "unassigned";
+	var S = "unassigned";
+	var W = "unassigned";
+
+	var sx = stoneToCheck.boardRef.bx - 1
+	var sy = stoneToCheck.boardRef.by - 1
+
+	if (sx == 0) {
+		W = "edge"
+	}
+	if (sy == 0) {
+		N = "edge"
+	}
+	if (sx == 8) {
+		E = "edge"
+	}
+	if (sy == 8) {
+		S = "edge"
+	}
+
+	var xN = gameState[sx][sy - 1]
+	var xE = gameState[sx + 1][sy]
+	var xS = gameState[sx][sy + 1]
+	var xW = gameState[sx - 1][sy]
+
+
+	if (N != "edge") {
+		if (xN != undefined) {
+			N = xN.colour
+		}
+	}
+
+	if (E != "edge") {
+		if (xE != undefined) {
+			E = xE.colour
+		}
+	}
+
+	if (S != "edge") {
+		if (xS != undefined) {
+			S = xS.colour
+		}
+	}
+
+	if (W != "edge") {
+		if (xW != undefined) {
+			W = xW.colour
+		}
+	}
+
+	var array = [N, E, S, W];
+
+
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] == "unassigned") {
+			lifePoints += 1
+		}
+	}
+
+	if (lifePoints == 0) {
+		deleteStone(stoneToCheck)
+	}
+}
+
+function reDrawStones() {
+	for (var i = 0; i < allStones.length; i++) {
+		drawStone(allStones[i])
+	}
+}
+
+function deleteStone(stone) {
+	sx = stone.boardRef.bx - 1
+	sy = stone.boardRef.by - 1
+	gameState[sx][sy] = undefined
+	for (var i = 0; i < allStones.length; i++) {
+		if (allStones[i].index == stone.index) {
+			allStones.splice(i, 1)
+			break;
+		}
+	}
+	drawBoard();
+	reDrawStones();
+}
+
+
+/*
+for (var i = 0; i < gridSize; i++) {
+		for (var j = 0; j < gridSize; j++) {
+			for (var k = 0; k < allStones.length; i++) {
+				
+			}
+		}
+	}
+*/
